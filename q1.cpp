ae2b51f57ad798b2f7c653934cffbf743151e609
Atifzaheer <128285262+rajAtif@users.noreply.github.com>
2023-03-19 13:53:05 +0500

    chapter 2 question

---
+++ q1.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int choice;
+	cout<<"please enter my choice";
+	cin>>choice;
+	int limit;
+	cout<<"please enter my limit ";
+	cin>>limit;
+	
+	for (int i=1 ; i<limit; i++)
+	cout<<choice*i<<"\t";
+
+	
+return 0;	
+}
+	
+	
+
+	
+
